fc45207c7d36f8f8d522ffa02bd35b5f9048df7d
Remco Bras <remco@bras-pan.eu>
2019-03-04 11:15:44 +0000

    Add test for larger categorical split

--- tests/python_package_test/test_decision_table.py
+++ tests/python_package_test/test_decision_table.py
@@ -3,7 +3,7 @@ import numpy as np
 import unittest
 
 class TestBasic(unittest.TestCase):
-    def test_basic_categorical_split(self):
+    def test_small_categorical_split(self):
         N = 1000
         X = np.zeros((N, 1))
         Y = np.zeros((N,))
@@ -19,10 +19,31 @@ class TestBasic(unittest.TestCase):
         booster = lgbm.Booster(train_set=ds, params={'tree_learner': learner, 'learning_rate': 1.0})
         booster.update()
 
-        booster.save_model('test_categorical_%s.txt' % (learner,), num_iteration=1)
-
         tree = booster.get_tree(0,0)
         self.assertEqual(1, tree.num_splits())
         self.assertEqual(0, tree.split_feature(0))
         self.assertTrue(tree.is_categorical(0))
         self.assertEqual(tree.categorical_threshold(0), [2])
+
+    def test_large_categorical_split(self):
+        N = 1000
+        X = np.zeros((N, 1))
+        Y = np.zeros((N,))
+
+        for i in range(100):
+            X[range(i, N, 100),:] = i
+            Y[range(i, N, 100)] = 100 if i % 2 == 1 else 0
+
+        ds = lgbm.Dataset(X, categorical_feature=[0]).construct()
+        ds.set_label(Y)
+
+        learner = 'table'
+        booster = lgbm.Booster(train_set=ds, params={'tree_learner': learner, 'learning_rate': 1.0, 'max_cat_threshold': 100, 'min_data_per_group': 1})
+        booster.update()
+
+        tree = booster.get_tree(0,0)
+        self.assertEqual(1, tree.num_splits())
+        self.assertEqual(0, tree.split_feature(0))
+        self.assertTrue(tree.is_categorical(0))
+        expected_split_features = [i for i in range(100) if i % 2 == 1]
+        self.assertEqual(expected_split_features, tree.categorical_threshold(0))
