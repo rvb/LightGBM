660c63e08b6bc7824e5e3f32aac809a6bb3c224b
Remco Bras <remco@bras-pan.eu>
2019-03-04 11:25:03 +0000

    Add unit test for 2-level split with one-hot categorical features.

--- tests/python_package_test/test_decision_table.py
+++ tests/python_package_test/test_decision_table.py
@@ -47,3 +47,38 @@ class TestBasic(unittest.TestCase):
         self.assertTrue(tree.is_categorical(0))
         expected_split_features = [i for i in range(100) if i % 2 == 1]
         self.assertEqual(expected_split_features, tree.categorical_threshold(0))
+
+    def test_small_categorical_twolevel(self):
+        N = 1000
+        X = np.zeros((N, 2))
+        Y = np.zeros((N,))
+
+        for i in range(4):
+            X[range(i, N, 4),0] = i
+
+        for i in range(2):
+            for j in range(4):
+                X[range(4*i+j, N, 8),1] = i
+
+        Y = (X[:,0] == 2)*100+(X[:,1] == 1)*20
+
+        #For this situation, the best approach is to first split on the first feature, moving category 2 to the left,
+        #then split on the second, moving category 1 to the left.
+        ds = lgbm.Dataset(X, categorical_feature=[0,1]).construct()
+        ds.set_label(Y)
+
+        learner = 'table'
+        booster = lgbm.Booster(train_set=ds, params={'tree_learner': learner, 'learning_rate': 1.0})
+        booster.update()
+
+        booster.save_model('test_categorical_2level_%s.txt' % (learner,), num_iteration=1)
+
+        tree = booster.get_tree(0,0)
+        self.assertEqual(3, tree.num_splits())
+        self.assertTrue(all(tree.is_categorical(i) for i in range(tree.num_splits())))
+        self.assertEqual(0, tree.split_feature(0))
+        self.assertEqual(1, tree.split_feature(1))
+        self.assertEqual(1, tree.split_feature(2))
+        self.assertEqual([2], tree.categorical_threshold(0))
+        self.assertEqual([1], tree.categorical_threshold(1))
+        self.assertEqual([1], tree.categorical_threshold(2))
